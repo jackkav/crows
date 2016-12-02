68e46d57f74e69bb0c4a4dd8cb76c748c1aa3cd7
Jack Kavanagh <jackkav@gmail.com>
2016-12-02 10:15:16 +0800

    adjusted names

--- src/model.js
+++ src/model.js
@@ -2,8 +2,9 @@ import mongoose from 'mongoose'
 mongoose.Promise = global.Promise
 mongoose.connect(process.env.DATABASE || 'mongodb://localhost/test')
 const Error = mongoose.model('error', {
-  source: String,
+  application: String,
   environment: String,
+  client: String,
   message: String,
   stack: String,
   addedOn: Date,
