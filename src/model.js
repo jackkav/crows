9af94859b41c21a662b254729103267d2d1b3db1
Jack Kavanagh <jackkav@gmail.com>
2016-12-05 17:53:37 +0800

    updated db model

--- src/model.js
+++ src/model.js
@@ -2,11 +2,15 @@ import mongoose from 'mongoose'
 mongoose.Promise = global.Promise
 mongoose.connect(process.env.DATABASE || 'mongodb://localhost/test')
 const Error = mongoose.model('error', {
-  application: String,  // 爱健康医生端
+  application: String,  // gtzh, patient, doctor, doctor-assistant
   environment: String,  // staging
   client: String,       // iOS,username,bundleVersion
   message: String,      // Error in main.js
   stack: String,        // Error stack
-  addedOn: Date,        // datetime of error
+  createdAt: Date,      // datetime of error
+  hospital: String,     // 北大医院
+  priority: String,     // crash, unhandled, handled
+  severity: String,     // warning, error
+  origin: String,       // client, server
 })
 export default Error
